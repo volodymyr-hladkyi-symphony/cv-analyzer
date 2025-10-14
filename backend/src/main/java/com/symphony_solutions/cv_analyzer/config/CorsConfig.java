6a8a39405e44668a343da8849497849637bd2d21
Volodymyr Hladkyi <volodymyr.hladkyi@symphony-solutions.com>
2025-10-14 10:14:21 +0300

    feat: Add CORS configuration to allow requests from the CV Analyzer UI

---
+++ backend/src/main/java/com/symphony_solutions/cv_analyzer/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.symphony_solutions.cv_analyzer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://cv-analyzer-ui.onrender.com/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
